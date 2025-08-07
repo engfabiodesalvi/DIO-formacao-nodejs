70c8fd2e22a6125e8a75233bff7d43df801d6a00
engfabiodesalvi <engfabiodesalvi@gmail.com>
2025-08-07 20:55:25 -0300

    file 03-projeto-mario-kart/src/index.js modifiled

--- 03-projeto-mario-kart/src/index.js
+++ 03-projeto-mario-kart/src/index.js
@@ -64,15 +64,24 @@ async function playRaceEngine(playersForRace){
 
         // Sortear bloco
         let block = await getRandomBlock()
+        // Definindo o bloco para teste
+        //block = "CONFRONTO";
         console.log(`Bloco: ${block}`);
+
+        //Definindo os pontos iniciais para teste
+        /*
+        for(let index=0; index < playersForRace.length; index++) {
+            playersForRace[index].PONTOS = Math.floor(Math.random() * 0);
+        }
+        */
 
         // rolar os dados
         //let diceResult1 = await rollDice();
         //let diceResult2 = await rollDice();
-        let diceResult = []
+        let diceResult = [];
         for (let index = 0; index < playersForRace.length; index++) {
             diceResult.push(await rollDice());
-            console.log(`DiceResult Jogador ${index+1} = ${diceResult[diceResult.length-1]}`);
+            //console.log(`DiceResult Jogador ${index+1} = ${diceResult[diceResult.length-1]}`);
         }        
 
         // teste de habilidade
@@ -80,14 +89,15 @@ async function playRaceEngine(playersForRace){
         //let totalTestSkill2 = 0;
         let totalTestSkill = [];
 
-
-        continue; // skip the rest of the code
-
         // Testando Blocos - if
         if(block == "RETA"){
-                totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
-                totalTestSkill2 = diceResult2 + character2.VELOCIDADE;
+                //totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
+                //totalTestSkill2 = diceResult2 + character2.VELOCIDADE;
+                for (let index = 0; index < playersForRace.length; index++) {
+                    totalTestSkill.push(diceResult[index] + playersForRace[index].VELOCIDADE);
+                }
 
+                /*
                 await logRollResult(
                     character1.NOME, 
                     "velocidade",
@@ -101,12 +111,27 @@ async function playRaceEngine(playersForRace){
                     diceResult2,
                     character2.VELOCIDADE
                 );
-        }          
+                */
+                for (let index = 0; index < playersForRace.length; index++) {
+                    await logRollResult(
+                        playersForRace[index].NOME, 
+                        "velocidade",
+                        diceResult[index],
+                        playersForRace[index].VELOCIDADE
+                    );
+                }               
+                console.log("");
+
+        }                 
 
         if(block == "CURVA"){
-                totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
-                totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
+                //totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
+                //totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
+                for (let index = 0; index < playersForRace.length; index++) {
+                    totalTestSkill.push(diceResult[index] + playersForRace[index].MANOBRABILIDADE);
+                }
 
+                /*
                 await logRollResult(
                     character1.NOME, 
                     "manobrabilidade",
@@ -119,16 +144,34 @@ async function playRaceEngine(playersForRace){
                     "manobrabilidade",
                     diceResult2,
                     character2.MANOBRABILIDADE
-                );            
-        }   
-
+                );
+                */         
+                for (let index = 0; index < playersForRace.length; index++) {
+                    await logRollResult(
+                        playersForRace[index].NOME, 
+                        "manobrabilidade",
+                        diceResult[index],
+                        playersForRace[index].MANOBRABILIDADE
+                    );
+                }         
+                console.log("");
+         
+        }        
+ 
         if(block == "CONFRONTO"){
             // Escopo de variáveis
-            let powerResult1 = diceResult1 + character1.PODER;
-            let powerResult2 = diceResult2 + character2.PODER;
+            //let powerResult1 = diceResult1 + character1.PODER;
+            //let powerResult2 = diceResult2 + character2.PODER;
+            let powerResult = [];
+            for (let index = 0; index < playersForRace.length; index++) {
+                powerResult.push(diceResult[index] + playersForRace[index].PODER);
+                //powerResult.push(10);
+            }
 
-            console.log(`${character1.NOME} confrontou com ${character2.NOME}! 🥊!`);
+            //console.log(`${character1.NOME} confrontou com ${character2.NOME}! 🥊!`);
+            console.log(await getMsgConfronto(playersForRace));
 
+            /*
             await logRollResult(
                 character1.NOME, 
                 "poder",
@@ -141,19 +184,33 @@ async function playRaceEngine(playersForRace){
                 "poder",
                 diceResult2,
                 character2.PODER
-            );       
+            ); 
+            */                
+            for (let index = 0; index < playersForRace.length; index++) {
+                await logRollResult(
+                    playersForRace[index].NOME, 
+                    "poder",
+                    diceResult[index],
+                    playersForRace[index].PODER
+                );
+            }  
+            console.log("");
+                            
+            //continue; // skip the rest of the code               
 
             // Diminuindo Ifs - If ternario
             /*character2.PONTOS -=
                 powerResult1 > powerResult2 && character2.PONTOS > 0 ? 1 : 0;*/
 
             // Condição Dupla - If Combinado
+            /*
             if (powerResult1 > powerResult2 && character2.PONTOS > 0) {
                 console.log(
                     `${character1.NOME} venceu o confronto! ${character2.NOME} perdeu 1 ponto 🐢!`
                 );
                 character2.PONTOS--;
             }
+            */
             
             /*if (powerResult1 > powerResult2) {
                 if (character2.PONTOS > 0) {
@@ -166,12 +223,14 @@ async function playRaceEngine(playersForRace){
                 powerResult1 < powerResult2 && character1.PONTOS > 0 ? 1 : 0;*/
 
             // Condição Dupla - If Combinado
+            /*
             if (powerResult1 < powerResult2 && character1.PONTOS > 0) {
                 console.log(
                     `${character2.NOME} venceu o confronto! ${character1.NOME} perdeu 1 ponto 🐢!`
                 );                
                 character1.PONTOS--;
             }
+            */
 
             /*if (powerResult1 < powerResult2) {
                 if (character1.PONTOS > 0) {
@@ -180,18 +239,103 @@ async function playRaceEngine(playersForRace){
             }*/   
             
             // Diminuindo Ifs - If ternario
+            /*
             console.log(
                 powerResult1 == powerResult2
                 ? "Confronto empatado! Nenhum ponto foi perdido!"
                 : ""
             );
+            */
             /*if (powerResult1 == powerResult2) {
                 console.log("Confronto empatado! Nenhum ponto foi perdido!");
-            }*/           
+            }*/ 
+
+
+            // Obtendo o maior valor de powerResult
+            let maxPower = 0;
+            for(index = 0; index < playersForRace.length; index++) {
+                // if Ternario
+                maxPower = powerResult[index] > maxPower ? 
+                    powerResult[index] : maxPower
+            }
+
+            // Identificando os vencedores e perdedores da etapa!
+            let vencedores = [];
+            let perdedores = [];            
+            for(index = 0; index < playersForRace.length; index++) {
+                //let playerForRace = playersForRace[index];
+                if (powerResult[index] == maxPower) {                                        
+                    //vencedores.push({index: index, ...playerForRace}); // ... spread syntax
+                    vencedores.push({INDEX: index, ...playersForRace[index]}); // ... spread syntax
+                } else {
+                    //perdedores.push({INDEX: index, ...playerForRace});
+                    perdedores.push({INDEX: index, ...playersForRace[index]}); // ... spread syntax
+                }
+            }       
+            
+            // Empate
+            if (vencedores.length == playersForRace.length) {
+                console.log("Confronto empatado! Nenhum ponto foi perdido!");                
+            } else {
+                // Vencedores
+                let turbo = Math.random() > 0.5 ? true : false;
+                let msgPlayerNames = await getMsgWithPlayers(vencedores);
+                if (vencedores.length == 1) {
+                    if (turbo == true) {                        
+                        console.log(`${msgPlayerNames} venceu o confronto e ganhou 1 ponto de turbo 🌟!`);
+                    } else {
+                        console.log(`${msgPlayerNames} venceu o confronto!`)
+                    }                    
+                } else if (vencedores.length > 1) {
+                    if (turbo == true) {                        
+                        console.log(`${msgPlayerNames} venceram o confronto e ganharam 1 ponto de turbo 🌟!`);
+                    } else {
+                        console.log(`${msgPlayerNames} venceram o confronto!`)
+                    }
+                }
+               
+                for (let index = 0; index < vencedores.length; index++) {
+                    if (turbo == true) {
+                        //console.log(`${playersForRace[vencedores[index].INDEX].NOME} - ${playersForRace[vencedores[index].INDEX].PONTOS}`);
+                        playersForRace[vencedores[index].INDEX].PONTOS++;
+                        //console.log(`${playersForRace[vencedores[index].INDEX].NOME} - ${playersForRace[vencedores[index].INDEX].PONTOS}`);
+                    }
+                }
+
+                
+                // Perdedores
+                let cascoOuBomba = Math.random() > 0.5 ? 1 : 2; // 1 -> casco, 2 -> bomba   
+                for (let index = 0; index < perdedores.length; index++){ 
+                    //console.log(`${playersForRace[perdedores[index].INDEX].NOME} - ${playersForRace[perdedores[index].INDEX].PONTOS}`);             
+                    if (cascoOuBomba == 1) {
+                        if (playersForRace[perdedores[index].INDEX].PONTOS > 0) {
+                            console.log(
+                                `${playersForRace[perdedores[index].INDEX].NOME} perdeu 1 ponto 🐢!`
+                            ); 
+                            playersForRace[perdedores[index].INDEX].PONTOS--;
+                        }
+                    } else {
+                        if (playersForRace[perdedores[index].INDEX].PONTOS > 1) {
+                            console.log(
+                                `${playersForRace[perdedores[index].INDEX].NOME} perdeu 2 pontos 💣!`
+                            ); 
+                            playersForRace[perdedores[index].INDEX].PONTOS = playersForRace[perdedores[index].INDEX].PONTOS - 2 ;
+                        } else if (playersForRace[perdedores[index].INDEX].PONTOS > 0) {
+                            console.log(
+                                `${playersForRace[perdedores[index].INDEX].NOME} perdeu 1 ponto 💣!`
+                            ); 
+                            playersForRace[perdedores[index].INDEX].PONTOS-- ;       
+                        }                     
+                    }                  
+                    //console.log(`${playersForRace[perdedores[index].INDEX].NOME} - ${playersForRace[perdedores[index].INDEX].PONTOS}`);             
+                }
+            }
+           
         }           
 
         // Verificando o vencedor.
         // Winner - Else If's
+        /*
         if (totalTestSkill1 > totalTestSkill2) {
             console.log(`${character1.NOME} marcou um ponto!`);
             character1.PONTOS++;
@@ -199,6 +343,24 @@ async function playRaceEngine(playersForRace){
             console.log(`${character2.NOME} marcou um ponto!`);
             character2.PONTOS++;
         }
+        */
+
+        // Obtendo o maior valor de totalTestSkill
+        let maxSkill = 0;
+        for(index = 0; index < playersForRace.length; index++) {
+            // if Ternario
+            maxSkill = totalTestSkill[index] > maxSkill ? 
+                totalTestSkill[index] : maxSkill
+        }
+
+        // Identificando os vencedores da etapa!
+        
+        for(index = 0; index < playersForRace.length; index++) {
+            if (totalTestSkill[index] == maxSkill && maxSkill != 0) {
+                playersForRace[index].PONTOS++;
+                console.log(`${playersForRace[index].NOME} marcou um ponto!`);
+            } 
+        }               
 
         console.log("-----------------------------------------");
 
@@ -291,7 +453,7 @@ async function getPlayersForRace(numberOfPlayers) {
         console.log("");
         const userInput = prompt(`Definindo o jogador ${jogador}. Digite um número entre 1 a 6: `);     
         let numPlayer = parseInt(userInput, 10);
-        console.log(`Número do personagem: ${numPlayer}. ${(numPlayer == NaN)}`);
+        //console.log(`Número do personagem: ${numPlayer}. ${(numPlayer == NaN)}`);
 
         if (numPlayer !== NaN || numPlayer < 1 || numPlayer > players.length) {
             let playerSelected = false;
@@ -324,6 +486,44 @@ async function getPlayersForRace(numberOfPlayers) {
     console.log(``)
 
     return playersForRace;
+}
+
+// Creating a message with the players name
+async function getMsgWithPlayers(players) {
+    let msgWithPlayers = ""
+
+    for (let index = 0; index < players.length; index++) {
+        if (index == 0) {
+            //msgWithPlayers = `${msgWithPlayers} ${players[index].NOME}`;
+            msgWithPlayers = `${players[index].NOME}`;
+        }
+        else if (players.length - index >= 2)
+            msgWithPlayers = `${msgWithPlayers}, ${players[index].NOME}`;
+        else
+            msgWithPlayers = `${msgWithPlayers} e ${players[index].NOME}`;            
+    }
+
+    //msgWithPlayers =  "\n";
+
+    return msgWithPlayers;
+}
+
+// Creating a message with the players name
+async function getMsgConfronto(players) {
+    let msgConfronto = "Confronto entre"
+
+    for (let index = 0; index < players.length; index++) {
+        if (index == 0)
+            msgConfronto = `${msgConfronto} ${players[index].NOME}`;
+        else if (players.length - index >= 2)
+            msgConfronto = `${msgConfronto}, ${players[index].NOME}`;
+        else
+            msgConfronto = `${msgConfronto} e ${players[index].NOME}`;            
+    }
+
+    msgConfronto = msgConfronto + " começando 🥊!\n";
+
+    return msgConfronto;
 }
 
 // Creating a message with the players name
@@ -392,6 +592,8 @@ main();
     - NaN is never equal to itself: NaN == NaN and NaN === NaN both evaluate to false. This is a unique characteristic of NaN in JavaScript and other floating-point implementations.
     - NaN is never equal to any other value: Comparing NaN to any other number, string, boolean, or object using == or === will always result in false.
     - NaN is never unequal to itself: Consequently, NaN != NaN and NaN !== NaN both evaluate to true.    
+
+    continue; // skip the rest of the code   
 
 */    
 
